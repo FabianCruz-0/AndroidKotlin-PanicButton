c2a5d4f526ad664ae38b8941cf58a0362038935b
FabianCruz-0 <fabiancruzamador.0@gmail.com>
2022-03-28 02:28:05 -0600

    login and signout with user's MAC address

--- app/src/main/java/com/example/myapplication/LoginActivity.kt
+++ app/src/main/java/com/example/myapplication/LoginActivity.kt
@@ -1,11 +1,114 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_login.*
+import java.net.NetworkInterface
+import java.util.*
+
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private val db = FirebaseFirestore.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        auth = Firebase.auth
+
+        signInBtn.setOnClickListener {
+
+            val mEmail = emailEditText.text.toString().trim()
+            val mPassword = passwordEditText.text.toString().trim()
+            when {
+                mEmail.isEmpty() || mPassword.isEmpty() -> {
+                    Toast.makeText(
+                        baseContext, "Correo o contraseña incorrectos.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                else -> {
+                    SignIn(mEmail, mPassword)
+                }
+            }
+        }
+
+        signUpTextView.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+
     }
+
+    public override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        var userExist = false
+        val mac = getMac()
+
+        db.collection("usuarios").document(mac).get().addOnSuccessListener {
+            userExist = true
+            System.out.println("si esta")
+            val intent = Intent(this, MainActivity::class.java)
+            this.startActivity(intent)
+        }
+            .addOnFailureListener { exception ->
+                userExist = false
+                System.out.println("no esta")
+            }
+
+        if (currentUser != null) {
+            val intent = Intent(this, MainActivity::class.java)
+            this.startActivity(intent)
+        }
+    }
+
+    private fun SignIn(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this) { task ->
+            if (task.isSuccessful) {
+                Log.d("TAG", "signInWithEmail:success")
+                val intent = Intent(this, MainActivity::class.java)
+                this.startActivity(intent)
+                finish()
+            } else {
+                Log.w("TAG", "signInWithEmail:failure", task.exception)
+                Toast.makeText(
+                    baseContext, "Correo o contraseña incorrectos.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+    private fun getMac(): String {
+        try {
+            val all: List<NetworkInterface> =
+                Collections.list(NetworkInterface.getNetworkInterfaces())
+            for (nif in all) {
+                if (!nif.name.equals("wlan0", ignoreCase = true)) continue
+                val macBytes = nif.hardwareAddress ?: return ""
+                val res1 = StringBuilder()
+                for (b in macBytes) {
+                    //res1.append(Integer.toHexString(b & 0xFF) + ":");
+                    res1.append(String.format("%02X:", b))
+                }
+                if (res1.length > 0) {
+                    res1.deleteCharAt(res1.length - 1)
+                }
+                return res1.toString()
+            }
+        } catch (ex: Exception) {
+        }
+        return "02:00:00:00:00:00"
+    }
+
 }
